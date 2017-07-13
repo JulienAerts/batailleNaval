bf871bc9d8de95ec320c623601cc954f1312d511
Julien Aerts <0-pc@DESKTOP-0FM4N0U.HandsOnVision.com>
2017-07-12 21:14:53 -0400

    Initialisation avec Position des customBoutons

--- BattleShip/src/main/java/inf5153/battleship/interfaceGraphique/BoutonCustom.java
+++ BattleShip/src/main/java/inf5153/battleship/interfaceGraphique/BoutonCustom.java
@@ -1,13 +1,13 @@
 package inf5153.battleship.interfaceGraphique;
 import javax.swing.JButton;
-import inf5153.battleship.domain.*;
+import inf5153.battleship.domain.Position;
 
 public class BoutonCustom extends JButton {
 
     public Position position;
     
-    public BoutonCustom() {
-
+    public BoutonCustom(Position position) {
+        this.position = position;
     }
 }
 
