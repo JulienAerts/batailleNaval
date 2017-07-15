ff73388d93b11d8ffa9b33ecc237b79bf29a99f0
0-pc <0-pc@DESKTOP-0FM4N0U.HandsOnVision.com>
2017-07-14 21:37:47 -0400

    Rajout d'ue methode pour placer les bateaux du joueur1 provenant du UI (humain)

--- BattleShip/src/main/java/inf5153/battleship/domain/Humain.java
+++ BattleShip/src/main/java/inf5153/battleship/domain/Humain.java
@@ -1,5 +1,7 @@
 package inf5153.battleship.domain;
 
+import java.util.ArrayList;
+
 public class Humain extends Joueur {
 
 	private String identifiant;
@@ -18,6 +20,19 @@ public class Humain extends Joueur {
 	 */
 	public void setIdentifiant(String identifiant) {
 		this.identifiant = identifiant;
+	}
+        
+        public void placerBateauxUI(ArrayList<Position> positions) throws Exception {
+            
+            ArrayList<Bateau> bateaux = BateauFactory.genererTousLesBateaux();
+            
+            for(Bateau bateau : bateaux){
+                
+                ArrayList<Position> positionsBateau = new ArrayList<Position>(positions.subList(0, bateau.longueur));
+                positions.removeAll(positionsBateau);
+                carte.placerBateau(bateau, positionsBateau);
+
+            }
 	}
 
 }
