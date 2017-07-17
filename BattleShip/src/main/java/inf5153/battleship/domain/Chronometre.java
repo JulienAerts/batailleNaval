1b3c727a6d9305221adcb77c12ba72f7d3a20e55
P.O <P.O@PO-PC.local>
2017-07-17 14:42:42 -0400

    ajout de la classe chronometre

--- BattleShip/src/main/java/inf5153/battleship/domain/Chronometre.java
+++ BattleShip/src/main/java/inf5153/battleship/domain/Chronometre.java
@@ -1,9 +1,11 @@
+package inf5153.battleship.domain;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package inf5153.battleship.domain;
+
 
 /**
  *
